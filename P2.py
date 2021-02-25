ef7949737012bef87e80772478ea09065d1cf464
Roger-Mei <rogermei@umich.edu>
2021-02-25 11:44:00 -0500

    Part of rubric point 3

--- P2.py
+++ P2.py
@@ -44,13 +44,9 @@ ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.sh
 # Apply a distortion correction to raw images.                                                                            #
 ###########################################################################################################################
 
-# Read in an image from 'test_images'
-img = mpimg.imread('test_images/test1.jpg')
-
-# undistort the raw image
-undist = cv2.undistort(img, mtx, dist, None, mtx)
-
-# Define a undistort scheme work for later usage
+"""
+Useful function construction for this part
+"""
 def cal_undistort(img, objpoints, imgpoints):
     # Transform image into gray scale
     gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
@@ -63,9 +59,86 @@ def cal_undistort(img, objpoints, imgpoints):
     undist = cv2.undistort(img, mtx, dist, None, mtx)
     return undist
 
+# Read in an image from 'test_images'
+img = mpimg.imread('test_images/test1.jpg')
+
+# undistort the raw image
+undist = cal_undistort(img, objpoints, imgpoints)
+
 ###########################################################################################################################
 # Use color transforms, gradients, etc., to create a thresholded binary image.                                            #
 ###########################################################################################################################
+
+"""
+Useful function construction for this part
+"""
+def abs_sobel_thresh(img, orient='x', sobel_kernel=3, thresh=(0, 255)):
+    # Convert image to gray scale
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+
+    # Apply x or y gradient and take the absolute value
+    if orient == 'x':
+        abs_sobel = np.absolute(cv2.Sobel(gray, cv2.CV_64F, 1, 0))
+    if orient == 'y':
+        abs_sobel = np.absolute(cv2.Sobel(gray, cv2.CV_64F, 0, 1))
+
+    # Rescale back to 8 bit integer
+    scaled_sobel = np.uint8(255*abs_sobel/np.max(abs_sobel))
+
+    # Create a binary copy of scaled_sobel
+    grad_binary = np.zeros_like(scaled_sobel)
+
+    # Apply the threshold
+    grad_binary[(scaled_sobel > thresh[0]) & (scaled_sobel < thresh[1])] = 1
+
+    return grad_binary
+
+def mag_thresh(img, sobel_kernel=3, mag_thresh=(0, 255)):
+    # Convert image to gray scale
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+
+    # Take both sobel x and y grdients
+    sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize = sobel_kernel)
+    sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize = sobel_kernel)
+
+    # Calculate the gradient magnitude
+    gradmag = np.sqrt(sobelx**2 + sobely**2)
+
+    # Rescale back to 8 bit integer
+    scaled_mag = np.uint8(255*gradmag/np.max(gradmag))
+
+    # create a binary copy
+    mag_binary = np.zeros_like(scaled_mag)
+
+    # Apply the threshold
+    mag_binary[(scaled_mag > mag_thresh[0]) & (scaled_mag < mag_thresh[1])] = 1
+
+    return mag_binary
+
+def dir_threshold(img, sobel_kernel=3, dir_thresh=(0, np.pi/2)):
+    # Convert image to gray scale
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+
+    # Take both sobel x and y grdients
+    sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize = sobel_kernel)
+    sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize = sobel_kernel)
+
+    # Take the absolute value of the gradient direction
+    absgraddir = np.arctan2(np.absolute(sobely), np.absolute(sobelx))
+
+    # create a binary copy
+    dir_binary = np.zeros_like(absgraddir)
+
+    # Apply the threshold
+    dir_binary[(absgraddir > dir_thresh[0]) & (absgraddir < dir_thresh[1])] = 1
+    return dir_binary
+
+# Convert to HLS color space and separate S channel
+hls = cv2.cvtColor(undist, cv2.COLOR_RGB2HLS)
+s_channel = hls[:, :, 2]
+
+# Transform the original image to gray scale
+gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
 
 
 
