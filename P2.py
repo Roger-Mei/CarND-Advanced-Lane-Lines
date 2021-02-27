9d2bff3c47e6e1edb5a6135feec06c6d33a705bf
Roger-Mei <rogermei@umich.edu>
2021-02-26 19:52:45 -0500

    modified point 5

--- P2.py
+++ P2.py
@@ -192,8 +192,8 @@ def warp(img):
     # Four desired coordinates
     dst = np.float32(
         [[920, 0],
-         [920, 720],
-         [280, 720],
+         [920, img.shape[0]],
+         [280, img.shape[0]],
          [280, 0]])
 
     # Compute the perspective transformation matrix
@@ -273,9 +273,9 @@ def find_lane_pixels(warped_binary):
         win_xright_low = rightx_current - margin
         win_xright_high = rightx_current + margin
 
-        # Draw the windows on the visualization image
-        cv2.rectangle(out_img, (win_xleft_low, win_y_low), (win_xleft_high, win_y_high), (0, 255, 0), 2)
-        cv2.rectangle(out_img, (win_xright_low, win_y_low), (win_xright_high, win_y_high), (0, 255, 0), 2)
+        # # Draw the windows on the visualization image 
+        # cv2.rectangle(out_img, (win_xleft_low, win_y_low), (win_xleft_high, win_y_high), (0, 255, 0), 2)
+        # cv2.rectangle(out_img, (win_xright_low, win_y_low), (win_xright_high, win_y_high), (0, 255, 0), 2)
 
         # Identify the nonzero pixels in x and y within the window
         good_left_inds = ((nonzeroy > win_y_low) & (nonzeroy < win_y_high) 
@@ -310,9 +310,15 @@ def find_lane_pixels(warped_binary):
     return leftx, lefty, rightx, righty, out_img
 
 def fit_polynomial(warped_binary):
+    # Declare margin
+    margin = 100
+
     # Find out lane pixels first
     leftx, lefty, rightx, righty, out_img = find_lane_pixels(warped_binary)
-    
+
+    # create window image for visualization
+    window_img = np.zeros_like(out_img)
+
     # Find second order polynomial to each line
     left_fit = np.polyfit(lefty, leftx, 2)
     right_fit = np.polyfit(righty, rightx, 2)
@@ -332,23 +338,52 @@ def fit_polynomial(warped_binary):
     out_img[lefty, leftx] = [255, 0, 0]
     out_img[righty, rightx] = [0, 0, 255]
 
-    # Plots the left and right polynomials on the lane lines
-    plt.plot(left_fitx, ploty, color = 'yellow')
-    plt.plot(right_fitx, ploty, color = 'yellow')
+    # Generate a polygon to illustrate the search window area
+    # And recast the x and y points into usable format for cv2.fillPoly()
+    left_line_window1 = np.array([np.transpose(np.vstack([left_fitx-margin, ploty]))])
+    left_line_window2 = np.array([np.flipud(np.transpose(np.vstack([left_fitx+margin, 
+                              ploty])))])
+    left_line_pts = np.hstack((left_line_window1, left_line_window2))
+    right_line_window1 = np.array([np.transpose(np.vstack([right_fitx-margin, ploty]))])
+    right_line_window2 = np.array([np.flipud(np.transpose(np.vstack([right_fitx+margin, 
+                              ploty])))])
+    right_line_pts = np.hstack((right_line_window1, right_line_window2))
 
-    return out_img
+    # Draw the lane onto the warped blank image
+    cv2.fillPoly(window_img, np.int_([left_line_pts]), (0,255, 0))
+    cv2.fillPoly(window_img, np.int_([right_line_pts]), (0,255, 0))
+    result = cv2.addWeighted(out_img, 1, window_img, 0.3, 0)
+
+    # Plot the polynomial lines onto the image
+    plt.plot(left_fitx, ploty, color='yellow')
+    plt.plot(right_fitx, ploty, color='yellow')
+
+    return result
 
 """
 Implementation of this section
 """
 
-out_img = fit_polynomial(warped_binary)
+result = fit_polynomial(warped_binary)
 
-plt.imshow(out_img)
+plt.imshow(result)
 plt.show()
 
+###########################################################################################################################
+# Determine the curvature of the lane and vehicle position with respect to center.                                        #
+###########################################################################################################################
+"""
+Useful function construction for this part
+"""
 
-# Determine the curvature of the lane and vehicle position with respect to center.
+"""
+Implementation of this section
+"""
+
+
+
+
+
 # Warp the detected lane boundaries back onto the original image.
 # Output visual display of the lane boundaries and numerical estimation of lane curvature and vehicle position.
 
